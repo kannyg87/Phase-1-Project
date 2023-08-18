b926b8564715bc17af51b90a76fad9676631f79f
Kanny <kanny.ghafour87@gmail.com>
2023-08-17 22:01:28 -0500

    adding prettierrer

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const input = document.getElementById('taskInput')
-  const btn = document.getElementById('addButton')
-  const ul = document.getElementById('taskList')
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.getElementById("taskInput");
+  const btn = document.getElementById("addButton");
+  const ul = document.getElementById("taskList");
 
-  btn.addEventListener('click', ()=>{
-    let val = input.value
-    const li = document.createElement('li')
-    if (input.value !== ""){
-      li.innerHTML=`<div>${val}</div><button class ="delete-button">X</button>`
-        ul.appendChild(li);
-      }
-    ul.addEventListener('click', function(e){
-      if(e.target.classList.contains('delete-button')){
+  btn.addEventListener("click", () => {
+    let val = input.value;
+    const li = document.createElement("li");
+    if (input.value !== "") {
+      li.innerHTML = `<div>${val}</div><button class ="delete-button">X</button>`;
+      ul.appendChild(li);
+    }
+    ul.addEventListener("click", function (e) {
+      if (e.target.classList.contains("delete-button")) {
         e.target.parentElement.remove();
       }
-    })
-    input.value =''
+    });
+    input.value = "";
 
-    if(val === "cook"){
-        li.innerHTML=`<div>${val}</div><button id="addButton" class="recipe">How about Christmas pie?</button><button class ="delete-button">X</button>`
+    if (val === "cook") {
+      li.innerHTML = `<div>${val}</div><button id="addButton" class="recipe">How about Christmas pie?</button><button class ="delete-button">X</button>`;
     }
-
-  })
+  });
 });
-
